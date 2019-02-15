28463aed98e3c3316249968bdbc24b7c2d8d6c5c
crossaaron <atcbcrossx@gmail.com>
2019-02-15 12:32:56 -0800

    initial parcel setup

---
+++ Components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+       return (
+            <div>
+                <h1>Parcel Explore App</h1>
+                <p>Start of new Project</p>
+            </div>
+       )
+    }
+}
